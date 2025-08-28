52b358f63b85fc5926a073200c60279e534f9ee8
Luchoolopez <luchoolopez2810@gmail.com>
2025-08-27 23:10:44 -0300

    Haciendo Frontend

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+    return(
+        <div className="home-container">
+            <Header/>
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Home;
