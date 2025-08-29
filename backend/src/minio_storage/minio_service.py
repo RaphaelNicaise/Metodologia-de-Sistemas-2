1add243fbda24a8fba645a49ed64398918f7b72f
RaphaelNicaise <raphanicaise@gmail.com>
2025-08-29 19:52:45 -0300

    Funcionalidad base para guardar ticket

--- backend/src/minio_storage/minio_service.py
+++ backend/src/minio_storage/minio_service.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv('../.env')
+
 class MinioClient:
     """
     Clase singleton para gestionar la conexion con el cliente minio."""
@@ -23,3 +24,18 @@ class MinioClient:
                 secure=False
             )
         return cls._instance
+    
+    @classmethod
+    def guardar_ticket(cls, ticket_pdf):
+        """
+        Guarda el pdf que le mandes en el bucket tickets.
+        A definir si mandamos un path, un archivo directo, etc.
+        Depende mucho del momento de creacion del pdf.
+        Args:
+            ticket_pdf (_type_): _description_
+        """
+        cls.get_instance().fput_object(
+            "tickets", 
+            ticket_pdf.filename, 
+            ticket_pdf.path
+        )
