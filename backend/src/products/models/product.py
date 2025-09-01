e678f7a4fd8e842dd7d0ef6de8e2f4ee231ba06d
RaphaelNicaise <raphanicaise@gmail.com>
2025-09-01 05:36:16 -0300

    un producto puede tener varias categorias

--- backend/src/products/models/product.py
+++ backend/src/products/models/product.py
@@ -1,14 +1,15 @@
 class Product:
-    def __init__(self, id=None, name=None ,barcode=None, price=None, stock=None,  category=None):
+    def __init__(self, id=None, name=None, barcode=None, price=None, stock=None, url_image=None):
         self.id = id
         self.name = name
         self.barcode = barcode
         self.price = price
         self.stock = stock
-        self.category = category
+        self.url_image = url_image
 
     def __repr__(self):
-        return f"Product(id={self.id}, name={self.name}, barcode={self.barcode}, price={self.price}, stock={self.stock}, category={self.category})"
+        return (f"Product(id={self.id}, name={self.name}, barcode={self.barcode}, price={self.price}, "
+                f"stock={self.stock},  url_image={self.url_image})")
 
     def to_dict(self):
         return {
@@ -17,7 +18,7 @@ class Product:
             "barcode": self.barcode,
             "price": self.price,
             "stock": self.stock,
-            "category": self.category
+            "url_image": self.url_image
         }
     
     @classmethod
@@ -28,5 +29,5 @@ class Product:
             barcode=data.get("barcode"),
             price=data.get("price"),
             stock=data.get("stock"),
-            category=data.get("category")
+            url_image=data.get("url_image")
         )
--- backend/src/products/routes/products_routes.py
+++ backend/src/products/routes/products_routes.py
@@ -6,7 +6,8 @@ from products.services.products_service import ProductoService
 products_bp = Blueprint("products", __name__)
 service = ProductoService()
 
-@products_bp.route("/", methods=["GET"])
+@products_bp.route("/", methods=["GET"]) # 127.0.0.1:5000/products/
 def get_products():
     products = service.get_all_products()
     return jsonify([product.to_dict() for product in products]), 200
+
