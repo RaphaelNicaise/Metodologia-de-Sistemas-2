d0ebbe06033fc2ac9ff3beeff813af55f1d85f90
RaphaelNicaise <raphanicaise@gmail.com>
2025-09-02 21:31:54 -0300

    Refactor MinioClient para mejorar la inicialización y gestión de instancias

--- backend/src/minio_storage/minio_service.py
+++ backend/src/minio_storage/minio_service.py
@@ -9,26 +9,40 @@ class MinioClient:
     """
     Clase singleton para gestionar la conexion con el cliente minio."""
     _instance = None
-
-    @classmethod
-    def get_instance(cls):
-        if cls._instance is None:
+    
+    def __init__(self):
+        if not hasattr(self, "initialized"):
             access_key = os.getenv("MINIO_ROOT_USER")
             secret_key = os.getenv("MINIO_ROOT_PASSWORD")
             endpoint = os.getenv("MINIO_ENDPOINT")
-
-            cls._instance = Minio(
-                endpoint, 
+            self.client = Minio(
+                endpoint,
                 access_key=access_key,
                 secret_key=secret_key,
                 secure=False
             )
-        return cls._instance
+            MinioClient._instance = self
+            self.init_buckets()
+            self.initialized = True
+            print("MinIO Client operando correctamente")
+
+    def init_buckets(self):
+        """ Crea los buckets necesarios si no existen """
+        buckets = ["tickets", "images"]
+        for bucket in buckets:
+            if not self.client.bucket_exists(bucket):
+                self.client.make_bucket(bucket)
+
+    @classmethod
+    def get_instance(cls):
+        if cls._instance is None:
+            cls()
+        return cls._instance.client
     
     @classmethod
-    def guardar_ticket(cls, ticket_pdf):
+    def guardar_ticket(cls, ticket_pdf): # implementar
         """
-        Guarda el pdf que le mandes en el bucket tickets.
+        Guarda el pdf que le mandes en el bucket tickets. 
         A definir si mandamos un path, un archivo directo, etc.
         Depende mucho del momento de creacion del pdf.
         Args:
