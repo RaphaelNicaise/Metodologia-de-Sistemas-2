49f2994764342ad3ed9e39d40bb705d694efe727
RaphaelNicaise <raphanicaise@gmail.com>
2025-09-02 21:31:45 -0300

    Fix conn a Minio

--- backend/src/app.py
+++ backend/src/app.py
@@ -7,13 +7,13 @@ from src.providers.routes.providers_routes import providers_bp
 from src.sales.routes.sales_routes import sales_bp
 from src.users.routes.users_routes import users_bp
 
-from minio_storage import minio_service
+from minio_storage.minio_service import MinioClient
 from db import Database
 
-Database() # crea la unica instancia Singleton de la base de datos
+Database() # instancia del cliente de MySQL
+MinioClient() # instancia el singleton de MinIO 
 
 app = Flask(__name__)
-
 
 # register blueprints
 app.register_blueprint(accounting_bp)
