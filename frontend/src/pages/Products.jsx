ea4eb53cf6579b680a1cee820c9e8b9f1eb1fcca
¨LuchooLopez¨ <Luchoolopez2810@gmail.com>
2025-09-10 19:58:28 -0300

    creando los disenios de los productos

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,23 +1,34 @@
 import React from "react";
 import useGetProducts from "../hooks/useGetProducts";
 import ProductCard from "../components/ProductCard";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import '../styles/Products.css';
+
 
 const Products = () => {
     const { products, loading, error } = useGetProducts();
 
-    if(loading) return <p className="loading">Cargando productos...</p>
-    if(error) return <p className="error">Error: {error}</p>
+    if (loading) return <p className="loading">Cargando productos...</p>
+    if (error) return <p className="error">Error: {error}</p>
 
-    return(
-        <div className="post-container">
-            {products.length === 0 ? (
-                <p className="no-products">No hay Productos</p>
-            ) : (
-                products.map(product => (
-                    <ProductCard key={product.id}{...product}/>
-                ))
-            )}
-        </div>
+    return (
+        <>
+            <Header />
+            <div className="main-layout">
+                <Navbar/>
+                <div className="post-container">
+                    <h1>Productos</h1>
+                    {products.length === 0 ? (
+                        <p className="no-products">No hay Productos</p>
+                    ) : (
+                        products.map(product => (
+                            <ProductCard key={product.id}{...product} />
+                        ))
+                    )}
+                </div>
+            </div>
+        </>
     )
 }
 
