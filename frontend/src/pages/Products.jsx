887d6a6b67c083fcff5cb814ebc560ab61a4ce68
¨LuchooLopez¨ <Luchoolopez2810@gmail.com>
2025-09-23 19:40:40 -0300

    Formulario para anadir productos funciona

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -5,67 +5,57 @@ import Navbar from "../components/Navbar";
 import ProductsTable from "../components/ProductsTable";
 import SearchInput from "../components/SearchInput";
 import { FaPlus } from "react-icons/fa6";
+import CreateProduct from "../components/CreateProduct"; 
 import '../styles/Products.css'
 
 const Products = () => {
-    const { products, loading, error } = useGetProducts();
-    const [searchTerm, setSearchTerm] = useState('');
-    const [isOpen, setIsOpen] = useState(false);
+  const { products, loading, error } = useGetProducts();
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
 
-    const filteredProducts = products.filter(product =>
-        product.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+  const filteredProducts = products.filter(product =>
+    product.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
+  return (
+    <>
+      <Header />
+      <div className="main-layout">
+        <Navbar />
+        <div className="product-container">
+          <h1>Productos</h1>
 
-
-    return (
-        <>
-            <Header />
-            <div className="main-layout">
-                <Navbar />
-                <div className="product-container">
-                    <h1>Productos</h1>
-
-                    <div className="table-component">
-                        <div className="table-search-input">
-                            <SearchInput
-                                onSearch={setSearchTerm}
-                                placeholder="Buscar productos..."
-                            />
-                        </div>
-
-                        <div className="table-add-btn">
-                            <button onClick={() => setIsOpen(true)}><FaPlus /><span>Agregar Producto</span></button>
-                        </div>
-                    </div>
-
-                    {loading && <p>Cargando productos...</p>}
-                    {error && <p>Error: {error}</p>}
-
-                    {!loading && !error && filteredProducts.length > 0 && (
-                        <ProductsTable products={filteredProducts} />
-                    )}
-
-                    {!loading && !error && filteredProducts.length === 0 && (
-                        <p>No hay productos{searchTerm ? ` que coincidan con "${searchTerm}"` : ''}</p>
-                    )}
-                </div>
-
-                {isOpen && (
-                    <div className="add-product-container">
-                        <div className="form-add-product">
-                            <h2>Agregar producto</h2>
-                            <div className="form-product-inputs">
-                                
-                            </div>
-                            <button onClick={() => setIsOpen(false)}>Cerrar</button>
-                        </div>
-                    </div>
-
-                )}
+          <div className="table-component">
+            <div className="table-search-input">
+              <SearchInput
+                onSearch={setSearchTerm}
+                placeholder="Buscar productos..."
+              />
             </div>
-        </>
-    );
+
+            <div className="table-add-btn">
+              <button onClick={() => setIsOpen(true)}><FaPlus /><span>Agregar Producto</span></button>
+            </div>
+          </div>
+
+          {loading && <p>Cargando productos...</p>}
+          {error && <p>Error: {error}</p>}
+
+          {!loading && !error && filteredProducts.length > 0 && (
+            <ProductsTable products={filteredProducts} />
+          )}
+
+          {!loading && !error && filteredProducts.length === 0 && (
+            <p>No hay productos{searchTerm ? ` que coincidan con "${searchTerm}"` : ''}</p>
+          )}
+        </div>
+
+        {isOpen && (
+          <CreateProduct onClose={() => setIsOpen(false)} />
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Products;
