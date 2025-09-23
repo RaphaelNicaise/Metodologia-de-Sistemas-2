dc735bf7996fa67a4401e848db8fc73ef1af584b
RaphaelNicaise <raphanicaise@gmail.com>
2025-09-23 01:10:30 -0300

    Test Stock y Productos

--- backend/tests/integration/test_productos.py
+++ backend/tests/integration/test_productos.py
@@ -1,15 +1,4 @@
-# En ./tests/api_test/test_productos.py
-import sys
-sys.path.insert(0, "/app")  # Apunta al código montado en /app
-
 import pytest
-from src.app import create_app
-
-@pytest.fixture
-def client():
-    app = create_app(testing=True)
-    with app.test_client() as client:
-        yield client
 
 def test_get_products(client):
     response = client.get("/api/productos/")
