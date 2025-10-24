5753090d8b0af270a9e7eb34b1134294d134c82e
Luchoolopez <luchoolopez2810@gmail.com>
2025-10-24 17:30:23 -0300

    Arreglado problema con el localhost

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,17 @@
-/*import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    dedupe: ['react', 'react-dom'] //ayuda a evitar duplicados
-  },
-server: {
-  host: '0.0.0.0',
-  port: 5173,
-  watch: {
-    usePolling: true
-  }}
-})
-*/
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom']
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    watch: {
+      usePolling: true
+    }
+  }
 })
 
